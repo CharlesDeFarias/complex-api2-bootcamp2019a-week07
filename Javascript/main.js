192f29974bb6278a612ea29d4adec5a3607a5830
CharlesDeFarias <47393024+CharlesDeFarias@users.noreply.github.com>
2019-03-15 02:12:57 -0400

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -5,27 +5,44 @@ function alGen(e){
   fetch(`http://www.theaudiodb.com/api/v1/json/1/discography.php?s=${artist}`)
     .then(response=> response.json())
     .then(response => {
+      console.log(response)
       let albumRes= response.album;
       albumRes.forEach(album=>{
         const titleElement=document.createElement("li"),
-        addElement= document.getElementById("albumList");
+        addElement=document.querySelector("ul");
         titleElement.textContent= album.strAlbum;
         addElement.appendChild(titleElement)
       })
-  document.querySelector('li').addEventListener('click',alInfo)
-    //Need to find out how to make it so when I click on an album, it's inserted into the second fetched api url. So need to make a click event that clicks list items that don't exist until they're generated and then have that value go into the fetch url.
-    //let albumName = 
+    let albumName= document.querySelectorAll('li');
+    albumName.forEach(al=>al.addEventListener('click',alInfo))
     function alInfo(){
-    fetch(`http://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artist}&a=${albumName}`)
+      let albVar = this.innerText
+    fetch(`http://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artist}&a=${albVar}`)
       .then(response=> response.json())
       .then(response => {
-        console.log(albumName)
-
+        let term = (response.album[0].strWikipediaID );
+        wiki(term);
       })
     }
-
     })
     .catch(err=>{
       console.error(err.message)
     })
+    function wiki(x){
+      title= x;
+  fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=${title}&format=json`)
+  .then(res => res.json())
+  .then(response =>{
+    console.log(response)
+    if(response[2][0] === [] || response[2][0] === '' || response[2][0]==="null"){
+          document.querySelector('p').textContent = 'No results, sorry. Wikipedia aint hip';
+        }else{
+          let description = response[2][0]
+          document.querySelector('p').textContent = description;
+        }
+      })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
+}
 }
